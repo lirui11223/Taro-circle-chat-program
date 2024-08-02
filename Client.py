2abd01da25f9a621b1caa5bc0e5b6c6639f1d4b9
lirui11223 <142644737+lirui11223@users.noreply.github.com>
2024-08-02 10:55:22 +0800

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,18 @@
+import socket
+import time
+
+HOST = '127.0.0.1'  # 服务器地址
+PORT = 65432        # 服务器端口
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        msg = input('请输入信息: ')
+        if msg == '退出':
+            break
+        s.sendall(msg.encode('utf-8'))
+        data = s.recv(1024)
+
+        print('消息:', repr(data.decode('utf-8')))
+print('欢迎再次使用聊天程序')
+time.sleep(10)
---
+++ Serve.py
@@ -0,0 +1,17 @@
+import socket
+
+HOST = '127.0.0.1'  # 服务器地址
+PORT = 65432        # 服务器端口
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()  # 等待客户端连接
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024).decode()
+            if not data:
+                break
+            print('Received', repr(data))
+            conn.sendall(data.encode() + b'\n')  # 发送回客户端
